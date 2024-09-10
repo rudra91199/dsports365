1815a0ba748a09028429e6f925d2f359a14d2300
rudra91199 <rskrrudra9111999@gmail.com>
2024-09-11 01:05:06 +0600

    marquee

--- src/components/TrendMarquee/TrendMarquee.jsx
+++ src/components/TrendMarquee/TrendMarquee.jsx
@@ -1,11 +1,19 @@
-import './trendMarquee.css'
+import "./trendMarquee.css";
+import Marquee from "react-fast-marquee";
 
 const TrendMarquee = () => {
   return (
-    <div className='trendMarquee container'>
-        <span>Trending Now</span>
+    <div className="trendMarquee container">
+      <span>TRENDING NOW</span>
+     
+        <Marquee pauseOnHover={true}>
+          <h2>লাহোর ২০০৯: ক্রিকেটের রক্তাক্ত প্রান্তর</h2>
+          <h2>বার্সেলোনায় ফিরেই ছাড়বেন নেইমার!</h2>
+          <h2>হঠাৎ ছাই হওয়া এক নক্ষত্র</h2>
+        </Marquee>
+  
     </div>
-  )
-}
+  );
+};
 
-export default TrendMarquee
+export default TrendMarquee;
