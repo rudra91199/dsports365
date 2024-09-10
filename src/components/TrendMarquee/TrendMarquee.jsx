3860b4ac1a2414a9a6402059b09675c305465830
rudra91199 <rskrrudra9111999@gmail.com>
2024-09-10 23:06:54 +0600

    container Edited

---
+++ src/components/TrendMarquee/TrendMarquee.jsx
@@ -0,0 +1,11 @@
+import './trendMarquee.css'
+
+const TrendMarquee = () => {
+  return (
+    <div className='trendMarquee container'>
+        <span>Trending Now</span>
+    </div>
+  )
+}
+
+export default TrendMarquee
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,9 +1,11 @@
 import TrendingPost from '../../components/TrendingPost/TrendingPost'
+import TrendMarquee from '../../components/TrendMarquee/TrendMarquee'
 import './news.css'
 
 const News = () => {
   return (
     <div className='news'>
+      <TrendMarquee/>
       <TrendingPost/>
     </div>
   )
