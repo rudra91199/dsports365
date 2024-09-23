1160dedf723606060753cabd6da3d990ba90dffa
rudra91199 <rskrrudra9111999@gmail.com>
2024-09-23 18:24:24 +0600

    search

--- src/pages/Shared/Navbar/Search.jsx
+++ src/pages/Shared/Navbar/Search.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { useFetch } from "../../../hooks/useFetch";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../../axios";
 
 const Search = () => {
   const [searchOpen, setSearchOpen] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const enableSearch = searchText.length > 2;
+
+  const { data: { result } = {}, isLoading } = useQuery({
+    queryKey: ["searchedProducts", searchText],
+    queryFn: () => makeRequest.get(`posts/getProductByQuery/${searchText}`),
+  });
 
   useEffect(() => {
     if (searchOpen === true) {
@@ -13,6 +24,9 @@ const Search = () => {
     }
   }, [searchOpen]);
 
+  console.log(result);
+  console.log(searchText.length);
+
   return (
     <div className="search">
       <IoSearch
@@ -21,16 +35,19 @@ const Search = () => {
           setSearchOpen(!searchOpen);
         }}
       />
-      <div
-        className={`searchBar ${searchOpen && "searchBarOpen"}`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <>
-          <input type="text" />
-          <button>
-            Search <MdOutlineKeyboardArrowRight />
-          </button>
-        </>
+      <div className={`searchContainer ${searchOpen && "searchContainerOpen"}`}>
+        <div className={`searchBar`} onClick={(e) => e.stopPropagation()}>
+          <>
+            <input
+              type="text"
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <button>
+              Search <MdOutlineKeyboardArrowRight />
+            </button>
+          </>
+        </div>
+        <div className="searchedProducts"></div>
       </div>
     </div>
   );
